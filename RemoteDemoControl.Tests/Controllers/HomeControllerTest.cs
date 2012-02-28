bdd2f82f1a8319470c6c505590c003a9a63e51a3
Scott Gilroy <sgilroy@mit.edu>
2012-02-28 11:07:01 -0500

    Initial commit.

---
+++ RemoteDemoControl.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoteDemoControl;
+using RemoteDemoControl.Controllers;
+
+namespace RemoteDemoControl.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void StartDemo()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.StartDemo() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ RemoteDemoControl/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+using RemoteDemoControl.Models;
+
+namespace RemoteDemoControl.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Demo state unknown.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult StartDemo()
+        {
+            ViewBag.Message = "Starting demo...";
+            DemoLaunchingClient client = new DemoLaunchingClient();
+            client.StartDemo();
+            return View("Index");
+        }
+
+        public ActionResult StopDemo()
+        {
+            ViewBag.Message = "Stopping demo...";
+            DemoLaunchingClient client = new DemoLaunchingClient();
+            client.StopDemo();
+            return View("Index");
+        }
+    }
+}
